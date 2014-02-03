eab0e6d594b94d2a6a8b3d4a7af7f212976cbfe4
Benktesh D Sharma <bsharma9@gatech.edu>
2014-02-03 10:33:10 -0800

    Cleaned code to allow multiple delimiter in command line arguements.

--- src/AverageSentenceLength.java
+++ src/AverageSentenceLength.java
@@ -1,47 +1,59 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 /**
- * Returns the average number length of the sentances contained in a text
- * file based on user's defined sentence delimitter and user's defined
+ * Returns the average number length of the sentences contained in a text
+ * file based on user's defined sentence delimiter and user's defined
  * minimum number of character length for word. The input file must be a
- * plain text file and langauge is English. Only one delimitter to separate 
+ * plain text file and language is English. Only one delimiter to separate 
  * sentences are allowed.   
  * <p>
  * There is only one method and parameters are supplied as arguments. 
- * There are four parameter requried. The parameters must be passed in the
+ * There are four parameter required. The parameters must be passed in the
  * exact order as defined below:
  * 
  * @param  filename a name of text file (or full path of the text file) 
  * 					The text file contains sentences written in English.
  * @param  -d		This parameter indicates that next parameter is 
- * 					sentence delimitter. 
- * @parameter -l	This parmaeter indicates that the next parameter is
+ * 					sentence delimiter. 
+ * @parameter -l	This parameter indicates that the next parameter is
  * 					minimum number of character to be considered a 'word'.
- * @return      	Average legnth of sentence for qualified words rounded down to 
+ * @return      	Average length of sentence for qualified words rounded down to 
  * 					nearest integer. 
  */
 
 
 
 
-=======
->>>>>>> origin/WorkingBranch
+//=======
+//>>>>>>> origin/WorkingBranch
 public class AverageSentenceLength {
 	
 	public static void main(String[] args) throws IOException{
 		
+		ArrayList<String> delimiterList = new ArrayList<String>();
+		
+		String delimiter="";
+		for (int i = 0; i < args.length; i++)
+		{	
+			if (args[i].equals("-d")) {
+				delimiter = delimiter + args[i+1];
+				i=i+1;
+			}
+		}
+		
+		System.out.println(delimiter);
+		
+		
 		String file = args[0];
-		String delimitter = args[2];
 		int minLength = Integer.parseInt(args[4]);
 		
 		Scanner reader = new Scanner (new FileInputStream(file));  
-		
-		if (delimitter.equals("."))  
-			delimitter = "\\.";
 		
 		String text = "";
 		int numberOfValidWords = 0;
@@ -56,18 +68,19 @@ public class AverageSentenceLength {
 			}
 		}
 		text = text.trim();
-		int numberOfValidSentences = (text.split(delimitter)).length;
+		System.out.println(text);
+		int numberOfValidSentences = (text.split("["+delimiter+"]")).length;
 		
-<<<<<<< HEAD
+//<<<<<<< HEAD
 		//System.out.println((int)(numberOfValidWords/numberOfValidSentences));
 		
 		System.out.println("Output: " + (int)(numberOfValidWords/numberOfValidSentences));
 	
-=======
-		System.out.println((int)(numberOfValidWords/numberOfValidSentences));
+//=======
+		//System.out.println((int)(numberOfValidWords/numberOfValidSentences));
 		
 		
->>>>>>> origin/WorkingBranch
+//>>>>>>> origin/WorkingBranch
 	
 	}
 
